814540102eefbfe7259dc9665cee431ee3ec0e48
Paul_Beckwith <303dog@gmail.com>
2020-09-24 12:40:05 -0600

    oniauth/session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :reviews
-  resources :users
+  get '/signup', to: 'users#new'
+  get '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: "sessions#destroy"
+  
+  #resources :reviews
+  
+  resources :users 
+  resources :reveiws, only: [:index]
+
+  get '/auth/:provider/callback', to: 'sessions#omniauth'
+  
+  resources :games do
+    resources :reviews
+  end
+  resources :reviews do 
+    resources :users
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
