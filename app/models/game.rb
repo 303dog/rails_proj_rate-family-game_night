2468ba0c1bfe4da0f898b805a103f14db9960c94
Paul_Beckwith <303dog@gmail.com>
2021-08-29 12:03:36 -0600

    made divs clickable for #show

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,6 +6,7 @@ class Game < ApplicationRecord
     validates :title, presence: true
     has_many :reviews
     has_many :users, through: :reviews
+    
 
     def self.alpha_order
         order('title')
