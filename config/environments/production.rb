cc8a7e1043f053437ba6a64090b2b86eb105efc0
Paul_Beckwith <303dog@gmail.com>
2021-10-19 11:09:28 -0600

    working on UX

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,12 +47,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com'
-  port: 587
-  domain: 'gmail.com'
-  authentication: 'plain'
-  enable_starttls_auto: true
-  user_name: ENV['GMAIL_USERNAME']
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'gmail.com',
+  authentication: 'plain',
+  enable_starttls_auto: true,
+  user_name: ENV['GMAIL_USERNAME'],
   password: ENV['GMAIL_PASSWORD']
 }
 
